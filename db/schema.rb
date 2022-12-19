ce56ae58d58d76ad065e35b58d5af62ed35d1bf0
Kasandra Darwin <kasandradarwin@gmail.com>
2022-12-19 18:20:20 +0000

    added additional spotlight photo to the db to showcase products for the spotlight section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_215101) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_171915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_215101) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "spotlight_photo"
     t.index ["user_id"], name: "index_businesses_on_user_id"
   end
 
